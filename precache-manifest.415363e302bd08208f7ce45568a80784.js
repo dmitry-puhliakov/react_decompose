52e73c47f7eaeb1c8fff6d05d3a61144e83a6783
Dmitry Pukhliakov <d.puhliakov@gmail.com>
2021-05-17 21:34:47 +0300

    Updates

---
+++ precache-manifest.415363e302bd08208f7ce45568a80784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c923c465172642d8f68e8caa152d014",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7411995ff52656406e6d",
+    "url": "./static/css/main.baa9ba56.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7411995ff52656406e6d",
+    "url": "./static/js/main.fd8b145e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
